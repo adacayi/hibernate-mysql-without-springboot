f9b2929e369d2fb698cb534b304801b5e62e43a2
Abdullah Sanver <a_sanver@hotmail.com>
2018-07-26 15:53:31 +0100

    Bismillahirrahmanirrahim. Hibernate with MySQL without Spring Boot.

---
+++ hibernate-mysql-without-springboot/src/test/java/com/sanver/trials/hibernate_del/AppTest.java
@@ -0,0 +1,14 @@
+package com.sanver.trials.hibernate_del;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	@Test
+	public void testApp() {
+		Assertions.assertTrue(true);
+	}
+}
